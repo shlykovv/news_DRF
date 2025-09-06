54a67fb11253e9246b6ec1ced435a69e5fbdc433
Vadim Shlykov <shlikov.vadim@gmail.com>
2025-09-06 19:15:04 +0300

    добавлена модель CustomUser в админку проекта

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(BaseUserAdmin):
+    list_display = ("email", "username", "first_name",
+                    "last_name", "is_active", "created_at")
+    list_filter = ("is_active", "is_staff", "is_superuser", "created_at")
+    search_fileds = ("email", "username", "first_name", "last_name")
+    ordering = ("-created_at",)
+    
+    fieldsets = (
+        (None, {"fields": ("email", "username", "password")}),
+        ("Personal Info", {"fields": ("first_name", "last_name", "avatar", "bio")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined", "created_at", "updated_at")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "username", "password1", "password2"),
+        })
+    )
+    readonly_fields = ("created_at", "updated_at")
+    
