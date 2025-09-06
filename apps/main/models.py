ba85fe2a0171c7539ae6d6a72bb7a7f2caa6c1ab
Vadim Shlykov <shlikov.vadim@gmail.com>
2025-09-06 20:23:24 +0300

    Добавлена модель Category для приложения main

--- apps/main/models.py
+++ apps/main/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.conf import settings
+from django.utils.text import slugify
+from django.urls import reverse
 
-# Create your models here.
+
+class Category(models.Model):
+    """Модель категорий для постов блога"""
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True, blank=True)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        db_table = "categories"
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+        ordering = ("-created_at",)
+    
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+    
