d30675d6ff890822c58f318013bb4be50284d7f1
Vadim Shlykov <shlikov.vadim@gmail.com>
2025-09-08 20:32:12 +0300

    Добавлены ручки по просмотру комментариев и детальному просмотру одного комментария, создание комментария, обновления и удаления. Также добавлены методы для просмотра комментариев определенного поста и получения ответов к комментариям

--- apps/comments/views.py
+++ apps/comments/views.py
@@ -1,3 +1,109 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, filters
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from .models import Comment
+from .serializers import (
+    CommentSerializer,
+    CommentCreateSerializer,
+    CommentUpdateSerializer,
+    CommentDetailSerializer
+)
+from .permissions import IsAuthorOrReadOnly
+from apps.main.models import Post
+
+
+class CommentListCreateAPIView(generics.ListCreateAPIView):
+    """Список и создание комментария"""
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly)
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter)
+    filterset_fileds = ("post", "author", "parent")
+    search_fields = ("content")
+    ordering_fields = ("created_at", "updated_at")
+    ordering = ("-created_at",)
+    
+    def get_queryset(self):
+        return Comment.objects.filter(is_active=True).select_related("author", "post", "parent")
+    
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return CommentCreateSerializer
+        return CommentSerializer
+
+
+class CommentDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    """Детальный просмотр, обновление и удаление комментария"""
+    queryset = Comment.objects.filter(is_active=True).select_related("author", "post")
+    serializer_class = CommentDetailSerializer
+    permissions_class = (IsAuthorOrReadOnly,)
+    
+    def get_serializer_class(self):
+        if self.request.method in ["PUT", "PATH"]:
+            return CommentUpdateSerializer
+        return CommentDetailSerializer
+    
+    def perform_destroy(self, instance):
+        """Мягкое удаление - помечаем как неактивный"""
+        instance.is_active = True
+        instance.save()
+
+
+class MyCommentsApiView(generics.ListAPIView):
+    """Список комментариев текущего пользователя"""
+    serializer_class = CommentSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter)
+    filterset_fields = ("post", "author", "parent")
+    search_fieds = ("content",)
+    ordering_fields = ("created_at", "updated_at")
+    ordering = ("-created_at")
+    
+    def get_queryset(self):
+        return Comment.objects.filter(
+            author=self.request.user).select_related("post", "parent")
+
+
+@api_view(["GET"])
+@permission_classes([permissions.AllowAny])
+def post_comments(request, post_id):
+    """Получить комментарий к определенному посту"""
+    post = get_object_or_404(Post, id=post_id, status="PUBLISHED")
+    # Получаем только основновные комментарии
+    comments = Comment.objects.filter(
+        post=post,
+        parent=None,
+        is_active=True
+    ).select_related(
+        "author").prefetch_related(
+            "replies__author").order_by("-created_at")
+    serializer = CommentDetailSerializer(comments, many=True, context={"request": request})
+    return Response({
+        "post": {
+            "id": post.id,
+            "title": post.title,
+            "slug": post.slug,
+        },
+        "comments": serializer.data,
+        "comments_count": post.comments.filter(is_active=True).count()
+    })
+
+
+@api_view("GET")
+@permission_classes([permissions.AllowAny])
+def comment_replies(request, comment_id):
+    """Получить ответы на комментарий"""
+    parent_comment = get_object_or_404(Comment, id=comment_id, status=True)
+    
+    replies = Comment.objects.filter(
+        parent=parent_comment,
+        is_active=True
+    ).select_related("author").order_by("-created_at")
+    
+    serializer = CommentSerializer(replies, many=True, context={"request": request})
+    return Response({
+        "parent_comment": CommentSerializer(parent_comment, context={"request": request}),
+        "replies": serializer.data,
+        "replies.count": replies.count()
+    })
